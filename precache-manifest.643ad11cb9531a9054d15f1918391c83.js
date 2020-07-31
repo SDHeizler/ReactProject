d98feec9fbc853b0e2b9dab54c9bb312c628d172
Scott Heizler <scottdheizler91@gmail.com>
2020-07-31 19:11:39 -0400

    Updates

---
+++ precache-manifest.643ad11cb9531a9054d15f1918391c83.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c681f4062a194ec7207e01f45e1781d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "855c0209a4c143f64c7b",
+    "url": "/static/css/main.baed954b.chunk.css"
+  },
+  {
+    "revision": "539d9ef222926879d0cc",
+    "url": "/static/js/2.d8220433.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d8220433.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "855c0209a4c143f64c7b",
+    "url": "/static/js/main.1c3b9596.chunk.js"
+  },
+  {
+    "revision": "1cebdf7130c159a0297a",
+    "url": "/static/js/runtime-main.c40c2c89.js"
+  },
+  {
+    "revision": "454f3d4dc7d1011e6699c94c5fdc79ef",
+    "url": "/static/media/TodosBackground.454f3d4d.jpg"
+  },
+  {
+    "revision": "90d3233befcdf9275faf90e0ecc4182a",
+    "url": "/static/media/checkmark.90d3233b.png"
+  },
+  {
+    "revision": "b37b05a713d8eebcbb475119e587859b",
+    "url": "/static/media/github.b37b05a7.png"
+  },
+  {
+    "revision": "dc8a298218b01cb39958e045e14036f8",
+    "url": "/static/media/remove.dc8a2982.png"
+  },
+  {
+    "revision": "a8a6123157ea92b98f344255b813c88e",
+    "url": "/static/media/todos.a8a61231.png"
+  },
+  {
+    "revision": "ff3d7423dfa07a77257930c641c740d1",
+    "url": "/static/media/user.ff3d7423.png"
+  },
+  {
+    "revision": "3d0bda57e018995c6cf378e1076abb89",
+    "url": "/static/media/xps-DYAf-8UTFN8-unsplash.3d0bda57.jpg"
+  }
+]);
